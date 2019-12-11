a42b768f6c37d3872a7a5d70d535dd07b8842ad1
Ross Kaffenberger <rosskaff@gmail.com>
2019-12-10 22:10:11 -0500

    Demo to show how to use images with Webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,14 @@
 import "core-js/stable"
 import "regenerator-runtime/runtime"
 
+import "../css/main.css"
+import "../css/sass.scss"
+
 import ujs from "@rails/ujs"
 
-ujs.start()
+import Taco from 'components/Taco'
 
+ujs.start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,5 +20,11 @@ ujs.start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require.context('../images', true)
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.body.insertAdjacentHTML('beforeend', Taco({ title: 'This picture of tacos is rendered from a JavaScript module.'}))
+})
 
 console.log('application.js loaded')
