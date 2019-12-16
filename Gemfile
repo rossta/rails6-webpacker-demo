f0b750090d1177218c501c5145e8582b2583326a
Ross Kaffenberger <rosskaff@gmail.com>
2019-12-16 14:20:35 -0500

    Demo active storage drag and drop

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'webpacker', '~> 4.0'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'active_storage_drag_and_drop'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,12 @@ import "core-js/stable"
 import "regenerator-runtime/runtime"
 
 import ujs from "@rails/ujs"
+import * as ActiveStorageDragAndDrop from "active_storage_drag_and_drop"
+window.ActiveStorageDragAndDrop = ActiveStorageDragAndDrop
 
 ujs.start()
 
+ActiveStorageDragAndDrop.start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
