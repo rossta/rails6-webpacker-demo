9770281699bd219d57fa00a4805d2dfa48418a0a
Ross Kaffenberger <rosskaff@gmail.com>
2020-04-14 21:29:27 -0400

    Add speed measure plugin example

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+const smp = new SpeedMeasurePlugin()
+
+module.exports = smp.wrap(environment.toWebpackConfig())
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+const smp = new SpeedMeasurePlugin()
+
+module.exports = smp.wrap(environment.toWebpackConfig())
