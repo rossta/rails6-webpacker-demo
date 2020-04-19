d8ee29db0886572aa22d00333a9d6ede413f9682
Ross Kaffenberger <rosskaff@gmail.com>
2020-04-19 05:46:51 -0400

    Add dotenv example

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,5 +4,16 @@ if (process.env.WEBPACK_ANALYZE === 'true') {
   const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
   environment.plugins.append('BundleAnalyzerPlugin', new BundleAnalyzerPlugin())
 }
+const dotenv = require('dotenv')
+
+const dotenvFiles = [
+  `.env.${process.env.NODE_ENV}.local`,
+  '.env.local',
+  `.env.${process.env.NODE_ENV}`,
+  '.env',
+]
+dotenvFiles.forEach((dotenvFile) => {
+  dotenv.config({ path: dotenvFile, silent: true })
+})
 
 module.exports = environment
