786511fe269e1c68cc08b622fd7d0f24abc9eae8
Ross Kaffenberger <rosskaff@gmail.com>
2020-05-17 09:24:19 -0400

    Add example to show tree shaking in production mode

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,9 @@ import ujs from "@rails/ujs"
 
 ujs.start()
 
+import { Row } from 'antd'
+
+console.log('Row', Row)
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,17 @@
 const { environment } = require('@rails/webpacker')
 
+// To view development bundle:
+// $ WEBPACK_ANALYZE=true RAILS_ENV=development NODE_ENV=development bin/webpack
+//
+// To view production bundle:
+// $ WEBPACK_ANALYZE=true RAILS_ENV=production NODE_ENV=production bin/webpack
+//
+// The development bundle will include the entire antd lib. The production build
+// should tree-shake unused modules and will be significantly smaller.
+//
+if (process.env.WEBPACK_ANALYZE) {
+  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+  environment.plugins.append('BundleAnalyzerPlugin', new BundleAnalyzerPlugin())
+}
+
 module.exports = environment
