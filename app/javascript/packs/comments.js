672e962c225e3e62758372bc19ea4af82c256a07
Ross Kaffenberger <rosskaff@gmail.com>
2020-05-19 08:43:34 -0400

    Add react example with multiple packs and splitChunks

---
+++ app/javascript/packs/comments.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import Comment from '../components/Comment'
+import render from '../render'
+
+render(<Comment author="Ross" body="I'm a Comment component!" />)
---
+++ app/javascript/packs/posts.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import Post from '../components/Post'
+import render from '../render'
+
+render(<Post author="Ross" body="I'm a Post component!" />)
