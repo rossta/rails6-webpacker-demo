e35c90b1e108ac39449a69803e7f407120fa9ae0
Ross Kaffenberger <rosskaff@gmail.com>
2020-05-21 22:16:02 -0400

    Update components with additional libraries

--- app/javascript/components/Post.jsx
+++ app/javascript/components/Post.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import moment from 'moment'
 
 export default function({ author, body }) {
   return (
-    <div>
+    <div style={{backgroundColor: 'aliceblue', padding: '1em'}}>
       <b>{author}</b> published a new Post: {body}
+      <div>This post was rendered at {moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
     </div>
   )
 }
