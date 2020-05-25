aeb04b8c5f136b1a546f03232e6baf8740da51be
Ross Kaffenberger <rosskaff@gmail.com>
2020-05-25 18:48:52 -0400

    Add webpack-dashboard plugin example

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,13 @@
 const { environment } = require('@rails/webpacker')
+const DashboardPlugin = require("webpack-dashboard/plugin");
 
 if (process.env.WEBPACK_ANALYZE === "true") {
   const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-  environment.plugins.append('BundleAnalyzerPlugin', new BundleAnalyzerPlugin())
+  environment.plugins.append('BundleAnalyzerPlugin', new BundleAnalyzerPlugin({
+    openAnalyzer: false
+  }))
 }
+
+environment.plugins.add('DashboardPlugin', new DashboardPlugin())
 
 module.exports = environment
