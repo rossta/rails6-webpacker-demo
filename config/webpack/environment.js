fe2275948279448b07b8a7dc1586f2bc48000f2e
Ross Kaffenberger <rosskaff@gmail.com>
2020-07-01 09:59:58 -0400

    Support chosen-js jQuery plugin with ProvidePlugin

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,14 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
 
 if (process.env.WEBPACK_ANALYZE === "true") {
   const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
   environment.plugins.append('BundleAnalyzerPlugin', new BundleAnalyzerPlugin())
 }
 
-environment.loaders.append('chosen', {
-  test: require.resolve('chosen-js'),
-  use: [{
-    loader: 'imports-loader',
-    options: 'jQuery=jquery,$=jquery,this=>window',
-  }],
-})
+environment.plugins.append('jQuery', new webpack.ProvidePlugin({
+  "jQuery": "jquery",
+  "$": "jquery"
+}))
 
 module.exports = environment
