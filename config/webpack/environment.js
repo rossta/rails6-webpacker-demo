47241701577c3864f8cf2f19bdac84908c550093
Ross Kaffenberger <rosskaff@gmail.com>
2020-08-31 08:45:54 -0400

    Ignore moment locales

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.append(
+  'IgnoreMomentLocales',
+  new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /(en-us|en-gb)/)
+)
 
 if (process.env.WEBPACK_ANALYZE === 'true') {
   const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
