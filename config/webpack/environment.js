f685ceca5b53546e1615dd9f61dfbd1805aaa4b0
Ross Kaffenberger <rosskaff@gmail.com>
2020-08-31 09:02:19 -0400

    Enable splitChunks optimization

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,6 +6,32 @@ environment.plugins.append(
   new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /(en-us|en-gb)/)
 )
 
+environment.splitChunks((config) => {
+  return {
+    ...config,
+    ...{
+      optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all',
+          name: true,
+          cacheGroups: {
+            vendors: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+            },
+            default: {
+              minChunks: 2,
+              priority: -20,
+              reuseExistingChunk: true,
+            },
+          },
+        },
+      },
+    },
+  }
+})
+
 if (process.env.WEBPACK_ANALYZE === 'true') {
   const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
   environment.plugins.append('BundleAnalyzerPlugin', new BundleAnalyzerPlugin())
