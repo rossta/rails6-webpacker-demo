98cdd1c46e904539deb5ac3cffb0b588eec7d76f
Ross Kaffenberger <rosskaff@gmail.com>
2020-09-01 15:42:32 -0400

    Reformat js with prettier

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,6 @@
 const { environment } = require('@rails/webpacker')
 
-if (process.env.WEBPACK_ANALYZE === "true") {
+if (process.env.WEBPACK_ANALYZE === 'true') {
   const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
   environment.plugins.append('BundleAnalyzerPlugin', new BundleAnalyzerPlugin())
 }
