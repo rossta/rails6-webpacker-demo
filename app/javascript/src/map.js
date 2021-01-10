649000185af50752e73d5fa1f07a591b13351cea
Ross Kaffenberger <rosskaff@gmail.com>
2021-01-10 17:20:41 -0500

    Add ol example
    Based on https://openlayers.org/en/latest/doc/tutorials/bundle.html

---
+++ app/javascript/src/map.js
@@ -0,0 +1,20 @@
+import { Map, View } from 'ol'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
+
+console.log('Loading map 1')
+
+document.addEventListener('DOMContentLoaded', () => {
+  const map = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    view: new View({
+      center: [0, 0],
+      zoom: 0,
+    }),
+  })
+})
